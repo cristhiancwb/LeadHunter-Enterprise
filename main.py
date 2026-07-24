ca2e7495bb684ea4e61589f8184a26929db8d1a1
cristhiancwb <80483689+cristhiancwb@users.noreply.github.com>
2026-07-24 10:10:30 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+LeadHunter Enterprise
+Versão: 0.1.0
+Autor: Cristhian Cabral + OpenAI
+
+Arquivo principal
+"""
+
+from app.gui import LeadHunterApp
+
+
+def main():
+    app = LeadHunterApp()
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
